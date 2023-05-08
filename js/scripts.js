8d8ae1717c9135b612b8b5894014b48eebf97e74
dynjir <60115950+dynjir@users.noreply.github.com>
2023-05-08 18:10:05 -0400

    trying out version control

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 */
 //
 // Scripts
-// 
+//
 
 
 function sendMail(){
@@ -13,7 +13,7 @@ function sendMail(){
     const templateID = "template_gyzn04g";
     var params = {
         name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
+        email: document.getElementById("userEmail").value,
         phone: document.getElementById("phone").value,
         subject: document.getElementById("subject").value,
         message: document.getElementById("message").value
@@ -23,7 +23,7 @@ function sendMail(){
         .send(serviceID, templateID, params)
         .then((res) => {
                 document.getElementById("name").value = "";
-                document.getElementById("email").value = "";
+                document.getElementById("userEmail").value = "";
                 document.getElementById("phone").value = "";
                 document.getElementById("subject").value = "";
                 document.getElementById("message").value = "";
